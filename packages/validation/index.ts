4bfb9ea83ce61a0805fe9001a926626c89ddc86c
jlucasbx <jlucasb16@gmail.com>
2026-01-30 11:32:52 -0300

    style: setting spaces to 2

--- packages/validation/index.ts
+++ packages/validation/index.ts
@@ -1,9 +1,9 @@
 import z from "zod"
 
 class UUIDValidator {
-    parse(uuid: string) {
-        return z.uuidv7().safeParse(uuid)
-    }
+  parse(uuid: string) {
+    return z.uuidv7().safeParse(uuid)
+  }
 }
 
 export const uuidValidator = new UUIDValidator()
