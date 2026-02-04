6df30e9a6c5612a2f1e5e12502a7496a7ea48853
jlucasbx <jlucasb16@gmail.com>
2026-02-04 17:16:41 -0300

    feat(dtos): exporting the QueryMoviesDTO

--- packages/dtos/src/index.ts
+++ packages/dtos/src/index.ts
@@ -2,5 +2,6 @@ export type { CreateUserDTO } from "./create-user.dto";
 export type { UpdateUserDTO } from "./update-user.dto";
 export type { CreateMovieDTO } from "./create-movie.dto";
 export type { UpdateMovieDTO } from "./update-movie.dto";
+export type { QueryMoviesDTO } from "./query-movies.dto";
 export { AgeRating, MovieStatus } from "@repo/validators";
 export { ErrorCode } from "./error-code.enum";
