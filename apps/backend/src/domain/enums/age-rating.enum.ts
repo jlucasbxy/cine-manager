b39d5f3b2d34f43aaba813c079f3ad9f5fc93c41
jlucasbx <jlucasb16@gmail.com>
2026-02-05 16:45:38 -0300

    refactor(backend): extract AgeRatingEnum and use in AgeRating value object

---
+++ apps/backend/src/domain/enums/age-rating.enum.ts
@@ -0,0 +1,8 @@
+export enum AgeRatingEnum {
+  L = "L",
+  TEN = "TEN",
+  TWELVE = "TWELVE",
+  FOURTEEN = "FOURTEEN",
+  SIXTEEN = "SIXTEEN",
+  EIGHTEEN = "EIGHTEEN",
+}
--- apps/backend/src/domain/value-objects/age-rating.value-object.ts
+++ apps/backend/src/domain/value-objects/age-rating.value-object.ts
@@ -1,21 +1,13 @@
 import z from "zod";
 import { InvalidAgeRatingError } from "@/domain/errors";
+import { AgeRatingEnum } from "@/domain/enums/age-rating.enum";
 
-const ageRatingValues = [
-  "L",
-  "TEN",
-  "TWELVE",
-  "FOURTEEN",
-  "SIXTEEN",
-  "EIGHTEEN"
-] as const;
-
-type AgeRatingValues = (typeof ageRatingValues)[number];
+const ageRatingValues = Object.values(AgeRatingEnum) as [AgeRatingEnum, ...AgeRatingEnum[]];
 
 export class AgeRating {
-  private readonly value: AgeRatingValues;
+  private readonly value: AgeRatingEnum;
 
-  private constructor(value: AgeRatingValues) {
+  private constructor(value: AgeRatingEnum) {
     this.value = value;
   }
 
@@ -24,14 +16,14 @@ export class AgeRating {
     if (!r.success) {
       throw new InvalidAgeRatingError();
     }
-    return new AgeRating(r.data);
+    return new AgeRating(value as AgeRatingEnum);
   }
 
-  static reconstitute(value: AgeRatingValues): AgeRating {
-    return new AgeRating(value);
+  static reconstitute(value: string): AgeRating {
+    return new AgeRating(value as AgeRatingEnum);
   }
 
-  public getValue(): AgeRatingValues {
+  public getValue(): AgeRatingEnum {
     return this.value;
   }
 
