ad08518fed131089fa47ddc265c9c5dc56879c47
jlucasbx <jlucasb16@gmail.com>
2026-02-11 11:55:22 -0300

    feat(backend): add getMimeType accessor to UploadKey value object

--- apps/backend/src/domain/value-objects/upload-key.value-object.ts
+++ apps/backend/src/domain/value-objects/upload-key.value-object.ts
@@ -2,16 +2,20 @@ import { Uuid, ImageMimeType } from "@/domain/value-objects";
 
 export class UploadKey {
 
-  private constructor(private readonly key: string) { }
+  private constructor(private readonly key: string, private mimeType: ImageMimeType) { }
 
   static create(userId: Uuid, mimeType: ImageMimeType): UploadKey {
     const id = Uuid.generate();
     const key = `uploads/${userId.toString()}/${id.toString()}${mimeType.toString()}`;
-    return new UploadKey(key);
+    return new UploadKey(key, mimeType);
   }
 
   static reconstitute(userId: Uuid, mimeType: ImageMimeType): UploadKey {
     return this.create(userId, mimeType);
+  }
+
+  public getMimeType(): ImageMimeType {
+    return this.mimeType
   }
 
   public toString(): string {
