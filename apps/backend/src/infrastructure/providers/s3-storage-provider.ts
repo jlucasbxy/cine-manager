8c30ad73289759652e3714f35abfeaeb436cb352
jlucasbx <jlucasb16@gmail.com>
2026-02-11 15:16:00 -0300

    refactor(backend): inject S3 config into S3StorageProvider via constructor

--- apps/backend/src/infrastructure/providers/s3-storage-provider.ts
+++ apps/backend/src/infrastructure/providers/s3-storage-provider.ts
@@ -1,27 +1,37 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import type { StorageProvider } from "@/application/interfaces/providers";
-import { s3Env } from "@/infrastructure/config/s3-env";
 import { UploadKey, Url } from "@/domain/value-objects";
 import { ImageUpload } from "@/domain/entities";
 
+interface S3StorageProviderConfig {
+  bucket: string;
+  region: string;
+  endpoint?: string;
+  forcePathStyle: boolean;
+  uploadUrlExpiresIn: number;
+}
+
 export class S3StorageProvider implements StorageProvider {
-  constructor(private readonly client: S3Client) {}
+  constructor(
+    private readonly client: S3Client,
+    private readonly config: S3StorageProviderConfig
+  ) {}
 
   async generateUploadUrl(uploadKey: UploadKey): Promise<ImageUpload> {
     const command = new PutObjectCommand({
-      Bucket: s3Env.S3_BUCKET,
+      Bucket: this.config.bucket,
       Key: uploadKey.toString(),
       ContentType: uploadKey.getMimeType().toString()
     });
 
     const uploadUrl = await getSignedUrl(this.client, command, {
-      expiresIn: s3Env.UPLOAD_URL_EXPIRES_IN
+      expiresIn: this.config.uploadUrlExpiresIn
     });
 
-    const fileUrl = s3Env.S3_FORCE_PATH_STYLE
-      ? `${s3Env.S3_ENDPOINT}/${s3Env.S3_BUCKET}/${uploadKey.toString()}`
-      : `https://${s3Env.S3_BUCKET}.s3.${s3Env.S3_REGION}.amazonaws.com/${uploadKey.toString()}`;
+    const fileUrl = this.config.forcePathStyle
+      ? `${this.config.endpoint}/${this.config.bucket}/${uploadKey.toString()}`
+      : `https://${this.config.bucket}.s3.${this.config.region}.amazonaws.com/${uploadKey.toString()}`;
 
     return ImageUpload.create({
       uploadKey,
--- apps/backend/src/main/factories/providers/make-storage-provider.ts
+++ apps/backend/src/main/factories/providers/make-storage-provider.ts
@@ -1,7 +1,15 @@
 import { S3StorageProvider } from "@/infrastructure/providers";
+import { s3Env } from "@/infrastructure/config/s3-env";
 import { singleton } from "@/main/factories/singleton";
 import { makeS3Client } from "@/main/factories/s3";
 
 export const makeStorageProvider = singleton(
-  () => new S3StorageProvider(makeS3Client())
+  () =>
+    new S3StorageProvider(makeS3Client(), {
+      bucket: s3Env.S3_BUCKET,
+      region: s3Env.S3_REGION,
+      endpoint: s3Env.S3_ENDPOINT,
+      forcePathStyle: s3Env.S3_FORCE_PATH_STYLE,
+      uploadUrlExpiresIn: s3Env.UPLOAD_URL_EXPIRES_IN
+    })
 );
