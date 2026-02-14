473ff7e311c861c5014f12b8e2fcf23bd5e5eaf3
jlucasbx <jlucasb16@gmail.com>
2026-02-14 20:29:39 -0300

    refactor(backend): wrap update user use case in transaction

--- apps/backend/src/application/use-cases/user/update-user.use-case.ts
+++ apps/backend/src/application/use-cases/user/update-user.use-case.ts
@@ -1,38 +1,42 @@
 import { Password, Uuid } from "@/domain/value-objects";
 import { UserNotFoundError } from "@/domain/errors";
-import type { UserRepository } from "@/application/interfaces/repositories";
-import type { HashProvider } from "@/application/interfaces/providers";
+import type {
+	HashProvider,
+	TransactionManager
+} from "@/application/interfaces/providers";
 import { UserMapper } from "@/application/mappers";
 import type { UpdateUserDTO, UserDTO } from "@repo/dtos";
 
 export class UpdateUser {
 	constructor(
-		private readonly userRepository: UserRepository,
-		private readonly hashProvider: HashProvider
+		private readonly hashProvider: HashProvider,
+		private readonly transactionManager: TransactionManager
 	) {}
 
 	async execute(userId: string, input: UpdateUserDTO): Promise<UserDTO> {
-		const id = Uuid.create(userId);
-		const user = await this.userRepository.findById(id);
+		return this.transactionManager.execute(async (repos) => {
+			const id = Uuid.create(userId);
+			const user = await repos.userRepository.findById(id);
 
-		if (!user) {
-			throw new UserNotFoundError();
-		}
+			if (!user) {
+				throw new UserNotFoundError();
+			}
 
-		const hashedPassword = input.password
-			? Password.reconstitute(
-					await this.hashProvider.hash(
-						Password.create(input.password).toString()
+			const hashedPassword = input.password
+				? Password.reconstitute(
+						await this.hashProvider.hash(
+							Password.create(input.password).toString()
+						)
 					)
-				)
-			: undefined;
+				: undefined;
 
-		await this.userRepository.updateById(id, {
-			name: input.name,
-			password: hashedPassword
+			await repos.userRepository.updateById(id, {
+				name: input.name,
+				password: hashedPassword
+			});
+
+			const updated = await repos.userRepository.findById(id);
+			return UserMapper.toDTO(updated!);
 		});
-
-		const updated = await this.userRepository.findById(id);
-		return UserMapper.toDTO(updated!);
 	}
 }
--- apps/backend/src/main/factories/use-cases/user/make-update-user.factory.ts
+++ apps/backend/src/main/factories/use-cases/user/make-update-user.factory.ts
@@ -1,7 +1,9 @@
 import { UpdateUser } from "@/application/use-cases/user";
-import { makeUserRepository } from "@/main/factories/repositories";
-import { makeHashProvider } from "@/main/factories/providers";
+import {
+	makeHashProvider,
+	makeTransactionManager
+} from "@/main/factories/providers";
 
 export function makeUpdateUser(): UpdateUser {
-	return new UpdateUser(makeUserRepository(), makeHashProvider());
+	return new UpdateUser(makeHashProvider(), makeTransactionManager());
 }
