082b7de6b4c253860d4f2b726ca5236e26d661a6
jlucasbx <jlucasb16@gmail.com>
2026-02-16 12:34:47 -0300

    feat(frontend): add enviroment variable to vite.config.ts

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,18 +1,22 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname, "VITE_");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src")
+      }
+    },
+    server: {
+      proxy: {
+        "/api": { target: env.VITE_API_URL, changeOrigin: true }
+      }
     }
-  },
-  server: {
-    proxy: {
-      "/api": { target: "http://localhost:3000", changeOrigin: true }
-    }
-  }
+  };
 });
