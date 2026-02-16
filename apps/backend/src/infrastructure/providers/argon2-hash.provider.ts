ba72340a9b3979aa10aad807c9a7b8830e8bdf16
jlucasbx <jlucasb16@gmail.com>
2026-02-16 11:19:27 -0300

    fix(backend): swap argon2.verify arguments to correct order

--- apps/backend/src/infrastructure/providers/argon2-hash.provider.ts
+++ apps/backend/src/infrastructure/providers/argon2-hash.provider.ts
@@ -15,6 +15,6 @@ export class Argon2HashProvider implements HashProvider {
     plaintext: string;
     hash: string;
   }): Promise<boolean> {
-    return argon2.verify(plaintext, hash);
+    return argon2.verify(hash, plaintext);
   }
 }
