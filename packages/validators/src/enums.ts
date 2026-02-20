87c854d71ebdcdca53999f6658161fc2a396e2f3
jlucasbx <jlucasb16@gmail.com>
2026-02-19 21:53:07 -0300

    refactor: replace TypeScript enums with as const objects

--- packages/validators/src/enums.ts
+++ packages/validators/src/enums.ts
@@ -1,17 +1,21 @@
-export enum AgeRating {
-  L = "L",
-  TEN = "TEN",
-  TWELVE = "TWELVE",
-  FOURTEEN = "FOURTEEN",
-  SIXTEEN = "SIXTEEN",
-  EIGHTEEN = "EIGHTEEN"
-}
+export const AgeRating = {
+  L: "L",
+  TEN: "TEN",
+  TWELVE: "TWELVE",
+  FOURTEEN: "FOURTEEN",
+  SIXTEEN: "SIXTEEN",
+  EIGHTEEN: "EIGHTEEN"
+} as const
 
-export enum MovieStatus {
-  RELEASED = "RELEASED",
-  POST_PRODUCTION = "POST_PRODUCTION",
-  IN_PRODUCTION = "IN_PRODUCTION",
-  PLANNED = "PLANNED",
-  CANCELED = "CANCELED",
-  RUMORED = "RUMORED"
-}
+export type AgeRating = (typeof AgeRating)[keyof typeof AgeRating]
+
+export const MovieStatus = {
+  RELEASED: "RELEASED",
+  POST_PRODUCTION: "POST_PRODUCTION",
+  IN_PRODUCTION: "IN_PRODUCTION",
+  PLANNED: "PLANNED",
+  CANCELED: "CANCELED",
+  RUMORED: "RUMORED"
+} as const
+
+export type MovieStatus = (typeof MovieStatus)[keyof typeof MovieStatus]
